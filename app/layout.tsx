76cf26affa172643e1c2ca7f9c947a3c76c5f09c
Kennedy12202 <kennedy12202@gmail.com>
2025-04-22 14:16:12 -0400

    footer fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className="bg-background text-foreground">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
+      <body className="bg-backgground text-foreground">
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
